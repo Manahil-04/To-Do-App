f7ad499ccd0a259227f8686040fcf2fd6d7c8234
Manahil-04 <102840081+Manahil-04@users.noreply.github.com>
2024-08-23 14:06:30 +0500

    Update input.jsx

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import './input.css';
 
-const Input = ({inputValue, handleChange, placeholder, styleclass}) => {
+const Input = ({inputValue, handleChange, placeholder}) => {
     return (
     <input
     value={inputValue}
     onChange={handleChange}
-    placeholder={placeholder}
-    className={styleclass} />
+    placeholder={placeholder} />
     )
 }
 
